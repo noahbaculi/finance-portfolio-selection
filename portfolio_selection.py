e8764cbe1041bcf7182ff32803bc22e3372b2c49
Noah Baculi <49008873+noahbaculi@users.noreply.github.com>
2019-06-26 07:47:54 -0500

    Add security category

--- portfolio_selection.py
+++ portfolio_selection.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
 # install requests
 # install requests-html
 
 # create symbols list with all securities to be considered
-symbols_list = ["KO", "SYG", "MUB", "MSEGX", "DIS", "V", "JPM", "MSFT", "AAPL"]
+symbols_list = ["KO", "SYG", "MUB", "MSEGX", "DBA", "DIS", "V", "JPM", "MSFT", "AAPL"]
 
 df = pd.DataFrame()
 pd.set_option("display.max_rows", 16)
@@ -34,24 +35,25 @@ for security_index in range(len(symbols_list)):
                 else:
                     security_type = "Fund..?"
             except:
-                raise NameError("An unknown error occured with the type of a fund security.")
-            # TODO attain fund category
+                raise SystemExit("An unknown error occured with the type of a fund security.")
+            security_category = info_block[0].find("span", {"class": "Fl(end)"}).text.strip()
 
             print("Success - Fund - %s" % symbols_list[security_index])
             print("Success - Fund Type - %s" % security_type)
+            print("Success - Fund Cat - %s" % security_category)
         else:
             print("%s had a Yahoo Finance Profile page that could not be analyzed to find a fund overview section." % symbols_list[security_index])
     except AttributeError:
         security_name = soup.find_all("div", {"class": "qsp-2col-profile Mt(10px) smartphone_Mt(20px) Lh(1.7)"})[0].find("h3").text.strip()
         security_type = "Company"
-
+        security_category = soup.find_all("span", {"class": "Fw(600)"})[0].text.strip()
         print("Success - Company - %s" % symbols_list[security_index])
         print("Success - Company Type - %s" % security_type)
-        # TODO attain company category
+        print("Success - Company Cat - %s" % security_category)
 
     df.at[security_index, "Security"] = security_name  # assign name to dataframe
     df.at[security_index, "Type"] = security_type  # assign type to dataframe
-    # TODO add security category to dataframe
+    df.at[security_index, "Category"] = security_category  # category name to dataframe
     print()
 
 print(df)  # print out dataframe
